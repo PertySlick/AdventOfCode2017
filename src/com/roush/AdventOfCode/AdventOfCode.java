f0667cf4bda86b5683339791d455c40af8eb91df
Tim Roush <perty_slick@hotmail.com>
2017-12-08 14:13:23 -0800

    Initial Commit - Advent Of Code Day 1 - Part 1: Inverse Captcha

---
+++ src/com/roush/AdventOfCode/AdventOfCode.java
@@ -0,0 +1,10 @@
+package com.roush.AdventOfCode;
+
+import com.roush.InverseCaptcha.InverseCaptcha;
+
+public class AdventOfCode {
+
+    public static void main(String[] args) {
+        new InverseCaptcha();
+    }
+}
